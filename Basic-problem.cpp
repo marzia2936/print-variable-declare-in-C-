6c7fafb9c1b9a15a1578a5a2abf216d134e138ba
marzia2936 <67289417+marzia2936@users.noreply.github.com>
2021-03-23 22:43:22 +0600

    Add files via upload

---
+++ Basic-problem.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+//printf("Hello Bangladesh");
+cout << "Hello Bangladesh"<<endl;
+//variable declare
+int n=5;
+//printf("%d",n);
+cout <<"variable="<<endl;
+cout <<n<<endl;
+return 0;
+}
